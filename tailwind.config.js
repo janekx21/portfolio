12211ae94b6a7ea8feeb97d1011787fc3614ba59
Janek Winkler <janekx21@gmail.com>
2023-07-18 23:37:31 +0200

    new styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,astro}"],
+  theme: {
+    colors: {
+      'dark-blue': 'rgba(35, 37, 48, 0.9)',
+      'orange': 'rgba(227, 184, 120, 1)',
+      'green': 'rgba(153, 187, 102, 1)',
+    },
+  },
+  plugins: [],
+}
+
