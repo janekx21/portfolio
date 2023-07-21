8c5ab3ecbca2d58b90695950d677fc0de9b72c74
Janek Winkler <janekx21@gmail.com>
2023-07-22 01:03:46 +0200

    new bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       'dark-blue': 'rgba(35, 37, 48, 0.9)',
       'orange': 'rgba(227, 184, 120, 1)',
       'green': 'rgba(153, 187, 102, 1)',
+      'white': '#fff',
+      'black': '#000',
     },
   },
   plugins: [],
