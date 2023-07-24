d72acc8b80dbb543e8105223652e978bb6656e2b
Janek Winkler <janekx21@gmail.com>
2023-07-24 18:00:13 +0200

    page transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js,astro}"],
   theme: {
     colors: {
-      'dark-blue': 'rgba(35, 37, 48, 0.9)',
+      'dark-blue': 'rgba(35, 37, 48, 0.7)',
       'orange': 'rgba(227, 184, 120, 1)',
       'green': 'rgba(153, 187, 102, 1)',
-      'white': '#fff',
-      'black': '#000',
+      'white': 'rgba(255,255,255,1)',
+      'black': 'rgba(0,0,0,1)',
     },
   },
   plugins: [],
