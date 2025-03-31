174e8e1890458cd0bf5a9805397fcbf221a15e9a
Janek Winkler <janekx21@gmail.de>
2025-03-31 17:29:33 +0200

    add animations

--- src/components/Window.svelte
+++ src/components/Window.svelte
@@ -1,36 +1,49 @@
 <script lang="ts">
   import { writable } from "svelte/store";
   import { spring } from "svelte/motion";
+  import { onMount, onDestroy } from 'svelte';
 
   let box;
   let scroll;
   let windowWidth;
   let windowHeight;
+  let offset = {x: 0, y: 0}
 
   let scrollSpring = spring(0,{
-    stiffness: .12,
+    stiffness: 0.05,//.12,
     damping: .25,
   });
+
   let size = spring(1, {
     stiffness: 0.3,
     damping: 0.5,
   });
+
   let pointing = spring({ x: 0, y: 0 }, {
     stiffness: 0.2,
     damping: 0.3,
   });
+
+  $: mobile = windowWidth < windowHeight;
+  
   $: width = (box?.clientWidth ?? 400)
   $: height = (box?.clientHeight ?? 400)
   $: center = {
     x: (box?.offsetLeft ?? 0) + width / 2,
     y: (box?.offsetTop ?? 0) + height / 2,
   };
-  $: rotateY = Math.min(Math.max($pointing.x + (center.x / windowWidth - 0.5) * -20, -30), 30);
-  $: rotateX = Math.min(Math.max($pointing.y + ((center.y - scroll) / windowHeight - 0.5) * 20 - (scroll - $scrollSpring) * .4, -30), 30);
-  $: style = `transform: perspective(1000px) scale(${$size}) rotateY(${rotateY}deg) rotateX(${rotateX}deg); filter: brightness(${brightness})`;
+  $: positionOffsetX = Math.pow(((center.y - $scrollSpring) / windowHeight - 0.5), 2) * 2000 * side;
+
+  $: side = mobile ? Math.sin(center.y / 3000 - 0.5) : center.x / windowWidth - 0.5; // * Math.sign(center.y / windowHeight - 0.5)
+
+  $: rotateY = Math.min(Math.max($pointing.x + (center.x / windowWidth - 0.5) * -20, -30), 30) + offset.y * offsetIntensity;
+  $: rotateX = Math.min(Math.max($pointing.y + ((center.y - scroll) / windowHeight - 0.5) * 20 - (scroll - $scrollSpring) * .4, -30), 30) + offset.x * offsetIntensity;
+  $: style = `transform: perspective(1000px) scale(${$size}) rotateY(${rotateY}deg) rotateX(${rotateX}deg) translateX(${positionOffsetX}px); filter: brightness(${brightness})`;
   $: normalizedSize =1// Math.sqrt(width * height) * .004
-  $: shineStyle = `background: radial-gradient(500px 1200px at ${rotateX * 10 + width/2}px ${rotateY * 10 + height/2 + 250}px , rgba(255,255,255,15%) 0%, rgba(255,255,255,5%) 30%, rgba(255,255,255,0) 100%);`
+  $: shineStyle = `background: radial-gradient(500px 1200px at ${(rotateY - 5) * -20 + width/2}px ${rotateX * 20 + height/2 + 250}px , rgba(255,255,255,15%) 0%, rgba(255,255,255,5%) 30%, rgba(255,255,255,0) 100%);`
   $: brightness = Math.tanh(-rotateX * .03) * -.5 + 1;
+
+  $: offsetIntensity = 1 - Math.atan(Math.abs($scrollSpring - scroll) + (($size - 1) * 10)) / (Math.PI / 2);
 
   function rotate({ layerX, layerY }) {
     let { clientWidth, clientHeight } = box;
@@ -46,6 +59,23 @@
     pointing.set({ x: 0, y: 0 });
   }
   // backdrop-blur-xl 
+
+
+  let rotateTimer
+  onMount(() => {
+    rotateTimer = setInterval(() => {
+      offset = {
+        x: Math.sin(Date.now() * 0.002 + center.x) * 8,
+        y: Math.cos(Date.now() * 0.002 + center.y) * 4
+      }
+    }, 16)
+  })
+
+
+  onDestroy(() => {
+    clearInterval(rotateTimer)
+  })
+  
 </script>
 
 <svelte:window bind:scrollY={scroll} bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} />
