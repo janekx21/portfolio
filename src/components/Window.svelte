9b677b3b480e4217a8b6051c508ad7e8812fa727
Janek Winkler <janekx21@gmail.de>
2026-04-10 21:04:02 +0200

    better window comp

--- src/components/Window.svelte
+++ src/components/Window.svelte
@@ -32,23 +32,24 @@
     x: (box?.offsetLeft ?? 0) + width / 2,
     y: (box?.offsetTop ?? 0) + height / 2,
   };
-  $: positionOffsetX = Math.pow(((center.y - $scrollSpring) / windowHeight - 0.5), 2) * 2000 * side;
+  $: hideAmount= Math.pow(((center.y - $scrollSpring) / windowHeight - 0.5) * 2, 8);
+  $: positionOffsetX = hideAmount * (windowWidth * 0.5) * side;
 
-  $: side = mobile ? Math.sin(center.y / 3000 - 0.5) : center.x / windowWidth - 0.5; // * Math.sign(center.y / windowHeight - 0.5)
+  $: side = mobile ? Math.sin(center.y / windowHeight * 10) : center.x / windowWidth - 0.5; // * Math.sign(center.y / windowHeight - 0.5)
 
   $: rotateY = Math.min(Math.max($pointing.x + (center.x / windowWidth - 0.5) * -20, -30), 30) + offset.y * offsetIntensity;
   $: rotateX = Math.min(Math.max($pointing.y + ((center.y - scroll) / windowHeight - 0.5) * 20 - (scroll - $scrollSpring) * .4, -30), 30) + offset.x * offsetIntensity;
-  $: style = `transform: perspective(1000px) scale(${$size}) rotateY(${rotateY}deg) rotateX(${rotateX}deg) translateX(${positionOffsetX}px); filter: brightness(${brightness})`;
+  $: opacity = 1 - Math.abs(hideAmount);
+  $: style = `transform: perspective(1000px) scale(${$size}) rotateY(${rotateY}deg) rotateX(${rotateX}deg) translateX(${positionOffsetX}px); filter: brightness(${brightness});opacity: ${Math.floor(opacity * 100)}%;`;
   $: normalizedSize =1// Math.sqrt(width * height) * .004
   $: shineStyle = `background: radial-gradient(500px 1200px at ${(rotateY - 5) * -20 + width/2}px ${rotateX * 20 + height/2 + 250}px , rgba(255,255,255,15%) 0%, rgba(255,255,255,5%) 30%, rgba(255,255,255,0) 100%);`
   $: brightness = Math.tanh(-rotateX * .03) * -.5 + 1;
 
   $: offsetIntensity = 1 - Math.atan(Math.abs($scrollSpring - scroll) + (($size - 1) * 10)) / (Math.PI / 2);
 
-  function rotate({ layerX, layerY }) {
-    let { clientWidth, clientHeight } = box;
-    let x = (layerX / clientWidth - 0.5) * 2 * 10;
-    let y = (layerY / clientHeight - 0.5) * 2 * -10;
+  function rotate({ pageX, pageY }) {
+    let x = ((pageX - center.x) / width) * 2 * 10;
+    let y = ((pageY - center.y) / height) * 2 * -10;
     pointing.set({ x, y });
   }
 
