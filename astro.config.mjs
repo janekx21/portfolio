7b4f8182abf2b7fe8b1b95e83d515dabae97df71
Janek Winkler <janekx21@gmail.de>
2026-04-12 22:01:04 +0200

    update stuff + first blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [
     tailwind(),
     svelte(),
+    mdx(),
+    sitemap(),
     icon({
       include: {
         mdi: ["github", "email"],
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,7 @@
+---
+title: First Post
+description: This is my first post yay
+pubDate: 'Apr 21 2026'
+---
+
+Some post
